b7499847771cb34ca824f620f58188936e8777f6
Rohit <rohit_kumar@gmail.com>
2021-09-20 16:06:53 +0530

    Added  class task

--- src/Point.ts
+++ src/Point.ts
@@ -1,1 +1,33 @@
-export class Point {}
+export class Point {
+    private x:number;
+    private y:number;
+    constructor();
+    constructor(x,y);
+    constructor(x=0,y=0) {
+        this.x=x;
+        this.y=y;
+    }
+    
+    
+    public toString():string{
+        return `"(${this.x},${this.y})"`;
+    }
+    
+    public distance(other:Point):number;
+    public distance(x:number,y:number):number;
+    public distance():number;
+   
+    public distance(x?:number|Point,y?:number):number{
+        if(!x && !y){
+            return Math.sqrt(Math.pow(this.x,2)+Math.pow(this.y,2));
+        }
+        if((x && typeof x == "number") && (y && typeof y == "number")){
+            return Math.sqrt(Math.pow((this.x-x),2)+Math.pow((this.y-y),2));
+        }
+        if(x && typeof x == 'object'){
+            return Math.sqrt(Math.pow((this.x-x?.x),2)+Math.pow((this.y-x?.y),2));
+        }
+        
+     }
+    
+}
--- src/Triangle.ts
+++ src/Triangle.ts
@@ -1,3 +1,36 @@
+import { Point } from './Point';
 import { Shape } from './Shape';
 
-export class Triangle extends Shape {}
+export class Triangle extends Shape {
+    
+    private indices:Point[]
+    
+    constructor(indices:Point[]){
+        super(indices);
+        if(indices.length!=3){
+            throw new Error('Please Enter 3 indices of triangle')
+        }
+        this.indices=indices
+    }   
+    toString():string{
+        return `Triangle[v1=${this.indices[0]},v2=${this.indices[1]},v3=${this.indices[2]}]`;
+        
+    }
+    
+    getType():string{
+      let type="";
+      if(this.indices.length===3){
+          if(this.getPerimeter(this.indices[0])===this.getPerimeter(this.indices[1]) &&
+             this.getPerimeter(this.indices[1])=== this.getPerimeter(this.indices[2])){
+             type="equilateral triangle";   
+          }else if(this.getPerimeter(this.indices[0]) != this.getPerimeter(this.indices[1]) &&
+                   this.getPerimeter(this.indices[1]) != this.getPerimeter(this.indices[2]) &&
+                   this.getPerimeter(this.indices[0]) != this.getPerimeter(this.indices[2])){
+            type="scalene triangle"; 
+          }else{
+            type="isosceles triangle"; 
+          }
+      }
+      return type;
+    }
+}
